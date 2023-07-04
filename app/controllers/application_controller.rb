102b4ae816a4bf2d974df10509d1180efe80b850
kwame bio <kwameagyemang73@gmail.com>
2023-07-04 18:19:24 +0000

    feature: error handling from standard error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,29 @@
 class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token, raise: false
-
-    include Authenticable
-
-
-    
-
+    skip_before_action :verify_authenticity_token
   
-    # def test
-    #   render json: { message: 'OK' }, status: :ok
-    # end
+    include Authenticable
+  
+    rescue_from StandardError, with: :error_handler
+
+    def error_handler(error)
+        case error
+        when ActionController::ParameterMissing
+          render json: { error: error.message }, status: :bad_request
+        when ActiveRecord::RecordNotFound
+          render json: { error: error.message }, status: :not_found
+        when ActiveRecord::RecordInvalid
+          render json: { error: error.message }, status: :bad_request
+        when JWT::VerificationError
+          render json: { error: "Invalid token" }, status: :unauthorized
+        when JWT::ExpiredSignature
+          render json: { error: "Token has expired" }, status: :unauthorized
+        when JWT::DecodeError
+          render json: { error: error.message }, status: :unauthorized
+        when ActionDispatch::Http::Parameters::ParseError
+          render json: { error: error.message }, status: :bad_request
+        else
+          render json: { error: error.message }, status: :internal_server_error
+        end
+    end
 end
+  
